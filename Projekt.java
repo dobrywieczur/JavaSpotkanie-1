467ec8aded18a274fc57a2b572c4f0136683b04c
karol <dsw47801@student.dsw.edu.pl>
2022-03-10 21:46:05 +0100

    123

--- Projekt.java
+++ Projekt.java
@@ -1,9 +1,12 @@
-package com.company;
+package com.zbackendu.main;
 
 public class Main {
-
     public static void main(String[] args) {
-	// write your code here
+    System.out.println("HELLO Z BACKENDU");
+
+
+
+
     }
 }
 
